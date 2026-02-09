4e77696bd859efb4df41a6a25aa9850cb59dc2ec
AndreottiEric <ericilvichingo2004@gmail.com>
2026-02-09 13:03:42 +0100

    Termina il CRUD, controller, service e repository per Customer

--- src/main/java/org/generation/italy/company/controller/CustomerController.java
+++ src/main/java/org/generation/italy/company/controller/CustomerController.java
@@ -1,30 +1,76 @@
 package org.generation.italy.company.controller;
 
 import org.generation.italy.company.dto.CustomerDTO;
+import org.generation.italy.company.dto.ProductDTO;
 import org.generation.italy.company.model.Customer;
+import org.generation.italy.company.model.Product;
 import org.generation.italy.company.service.abstraction.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import static org.generation.italy.company.dto.CustomerDTO.fromCustomer;
 
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
 
+    private CustomerService service;
+
     private final CustomerService customerService;
 
+    @Autowired
     public CustomerController(CustomerService customerService){
         this.customerService = customerService;
     }
 
     @PostMapping
-    public ResponseEntity<Customer> create(@RequestBody Customer c) {
-        return ResponseEntity.ok(customerService.create(c));
+    public ResponseEntity<CustomerDTO> create(@RequestBody CustomerDTO customerDto) {
+        Customer customer = customerDto.toEntity();
+        Customer created = service.create(customer);
+        CustomerDTO dto = fromCustomer(created);
+        URI location = URI.create("/api/products/" + created.getCustId());
+        return ResponseEntity.created(location).body(dto);
     }
 
     @GetMapping
-    public ResponseEntity<List<Customer>> findAll() {
-        return ResponseEntity.ok(customerService.findAll());
+    public List<CustomerDTO> findAll() {
+        List<Customer> lc = service.findAll();
+        return lc.stream().map(CustomerDTO::fromCustomer).toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable int id){
+        Optional<Customer> oc = service.findById(id);
+        if (oc.isPresent()){
+            return ResponseEntity.ok(fromCustomer(oc.get()));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable int id){
+        boolean deleted = service.deleteById(id);
+        if (deleted){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable int id, @RequestBody CustomerDTO customerDTO){
+        if (id != customerDTO.getCustId()){
+            return ResponseEntity.badRequest().body("L'id non corrisponde a quello desiderato.");
+        }
+        Customer customer = customerDTO.toEntity();
+        boolean updated = service.update(customer);
+        if (!updated){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(fromCustomer(customer));
     }
 }
--- src/main/java/org/generation/italy/company/dto/CustomerDTO.java
+++ src/main/java/org/generation/italy/company/dto/CustomerDTO.java
@@ -1,10 +1,97 @@
 package org.generation.italy.company.dto;
 
+import jakarta.persistence.Column;
+import org.generation.italy.company.model.Customer;
+
 public class CustomerDTO {
+
+    private Integer custId;
+    private String companyName;
+    private String contactName;
+    private String contactTitle;
+    private String address;
+    private String city;
+    private String region;
+    private String postalCode;
+    private String country;
+    private String phone;
+    private String fax;
 
     public CustomerDTO(){}
 
     public CustomerDTO(Integer custId, String companyName, String contactName, String contactTitle, String address,
                        String city, String region, String postalCode, String country, String phone, String fax){
+
+        this.custId = custId;
+        this.companyName = companyName;
+        this.contactName = contactName;
+        this.contactTitle = contactTitle;
+        this.address = address;
+        this.city = city;
+        this.region = region;
+        this.postalCode = postalCode;
+        this.country = country;
+        this.phone = phone;
+        this.fax = fax;
+    }
+
+    public Customer toEntity() {
+        Customer customer = new Customer(custId, companyName, contactName, contactTitle, address,
+                city, region, postalCode, country, phone, fax);
+        return customer;
+    }
+
+    public static CustomerDTO fromCustomer(Customer c){
+        return new CustomerDTO(c.getCustId(), c.getCompanyName(), c.getContactName(),
+                c.getContactTitle(), c.getAddress(), c.getCity(), c.getRegion(), c.getPostalCode(),
+                c.getCountry(), c.getPhone(), c.getFax());
+    }
+
+    public Integer getCustId() {
+        return custId;
+    }
+
+    public void setCustId(Integer custId) {
+        this.custId = custId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public String getContactTitle() {
+        return contactTitle;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getFax() {
+        return fax;
     }
 }
